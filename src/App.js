807561e0b2e6ee28571bdf6fcaac087a2d273338
DanielSantos0 <danielsantos.ee@gmail.com>
2025-06-03 01:03:14 -0300

    adicionando a foto do totinha

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './App.css';
 function App() {
   // Substitua essa data pela data que você quiser
   const targetDate = '2026-09-06T23:59:59';
+    const imageUrl = 'https://imgur.com/mjvHG0c.jpg'; // coloque o link da sua imagem aqui
+
 
 return (
     <div className="app-container">
+            <img src={imageUrl} alt="Logo" className="corner-logo" />
       <div className="content-wrapper">
-        <h1 className="title">Contagem Regressiva para o encontro anual da Yakuza Paraense 🎉</h1>
+        <h1 className="title">Contagem Regressiva para o encontro anual da Yakuza Paraense em Assis - SP 🎉</h1>
         <CountdownTimer targetDate={targetDate} />
       </div>
     </div>
